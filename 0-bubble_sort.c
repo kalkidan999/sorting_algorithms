fa9f3e5f7ce4936f6db0431e2e870055a4a9643f
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-14 10:30:35 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,23 +7,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-int tmp;
-int swapped;
+size_t j, i;
+size_t tmp;
 for (i = 0; i < size; i++)
 {
-swapped = 0;
 for (j = 0; j < size - 1 - i; j++)
 {
-if (array[j + 1] > array[j])
+if (array[j] > array[j + 1])
 {
 tmp = array[j];
 array[j] = array[j + 1];
 array[j + 1] = tmp;
-swapped = 1;
+print_array(array, size);
 }
 }
-if (!swapped)
-{
-break;
 }
 }
