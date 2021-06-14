bb5a08b10ac3406ad2100094ec49e3e1976b50e8
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-14 10:43:19 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,8 +7,10 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t j, i;
+size_t i, j;
 size_t tmp;
+if (size < 2)
+return;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size - 1 - i; j++)
