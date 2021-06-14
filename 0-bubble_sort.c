fab7e52e9dc90f9a5063a84dbf4d015262f51a9d
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-14 10:28:04 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,29 @@
-a
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in asc order
+ *
+ * @array: an array store
+ * @size: sizesssss
+ */
+void bubble_sort(int *array, size_t size)
+{
+int tmp;
+int swapped;
+for (i = 0; i < size; i++)
+{
+swapped = 0;
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j + 1] > array[j])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+swapped = 1;
+}
+}
+if (!swapped)
+{
+break;
+}
+}
